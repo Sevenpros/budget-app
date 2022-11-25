635b67024c3634282757bd72f7f615fb7d96e3d8
Sevenpros <sethbisoso@gmail.com>
2022-11-25 10:44:40 +0100

    Add functionality

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,8 @@ class TransactionsController < ApplicationController
 
   # GET /transactions or /transactions.json
   def index
-    @transactions = Transaction.where(category_id: params[:category_id])
+    @transactions = Transaction.includes(:category).where(category_id: params[:category_id])
+    @category = Category.find(params[:category_id])
   end
 
   # GET /transactions/1 or /transactions/1.json
